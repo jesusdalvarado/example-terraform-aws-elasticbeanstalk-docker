65baedc155a1828eaaf370785da1d7307fe06525
Jesus <jesusd.alvarado@gmail.com>
2020-09-14 19:43:43 +0200

    Creating AWS ElasticBeanstalk resources using terraform

---
+++ main.tf
@@ -0,0 +1,55 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-west-2"
+}
+
+resource "aws_elastic_beanstalk_application" "tftest" {
+  name          = "my-test"
+  description   = "some description"
+}
+
+resource "aws_iam_instance_profile" "test_profile" {
+  name = "test_profile"
+  role = aws_iam_role.role.name
+}
+
+resource "aws_iam_role" "role" {
+  name = "test_role"
+  path = "/"
+
+  assume_role_policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": "sts:AssumeRole",
+            "Principal": {
+               "Service": "ec2.amazonaws.com"
+            },
+            "Effect": "Allow",
+            "Sid": ""
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_elastic_beanstalk_environment" "prodenv" {
+  name                = "tf-test-name"
+  application         = aws_elastic_beanstalk_application.tftest.name
+  solution_stack_name = "64bit Amazon Linux 2 v3.1.0 running Ruby 2.7"
+
+  setting {
+    name = "IamInstanceProfile"
+    namespace = "aws:autoscaling:launchconfiguration"
+    value = aws_iam_instance_profile.test_profile.name
+  }
+}
