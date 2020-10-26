83166427104d5a107d7ca5b43d1e9f20d4364c64
Jesus <jesusd.alvarado@gmail.com>
2020-10-26 13:02:43 +0100

    Quick test

--- test.sh
+++ test.sh
@@ -1,4 +1,6 @@
+GH_PAT=$1
+
 docker build ./modules/webserver/ -t jesusdalvarado-example-image:latest
-echo "{{ secrets.GH_PAT }}" | docker login ghcr.io -u jesusdalvarado --password-stdin
+echo $GH_PAT | docker login ghcr.io -u jesusdalvarado --password-stdin
 docker tag jesusdalvarado-example-image:latest ghcr.io/jesusdalvarado/jesus-image:latest
 docker push ghcr.io/jesusdalvarado/jesus-image:latest
