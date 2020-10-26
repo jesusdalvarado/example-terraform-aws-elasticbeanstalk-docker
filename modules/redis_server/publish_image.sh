39958996e9abee16ffeaa7d46ca2ef9726e23714
Jesus <jesusd.alvarado@gmail.com>
2020-10-26 18:10:33 +0100

    Fixing problems with images and paths

--- modules/redis_server/publish_image.sh
+++ modules/redis_server/publish_image.sh
@@ -1,6 +1,6 @@
 GH_PAT=$1
 
-docker build . -t redis-jesus:latest
+docker build ./modules/redis_server/ -t redis-jesus:latest
 echo $GH_PAT | docker login ghcr.io -u jesusdalvarado --password-stdin
 docker tag redis-jesus:latest ghcr.io/jesusdalvarado/redis-jesus:latest
 docker push ghcr.io/jesusdalvarado/redis-jesus:latest
--- modules/webserver/publish_image.sh
+++ modules/webserver/publish_image.sh
@@ -1,6 +1,6 @@
 GH_PAT=$1
 
-docker build . -t jesusdalvarado-example-image:latest
+docker build ./modules/webserver/ -t jesusdalvarado-example-image:latest
 echo $GH_PAT | docker login ghcr.io -u jesusdalvarado --password-stdin
 docker tag jesusdalvarado-example-image:latest ghcr.io/jesusdalvarado/jesus-image:latest
 docker push ghcr.io/jesusdalvarado/jesus-image:latest
