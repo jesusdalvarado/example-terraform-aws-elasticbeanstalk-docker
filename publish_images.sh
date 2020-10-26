89c908ec52e5fb90092285b9d62544796f2e5dc3
Jesus <jesusd.alvarado@gmail.com>
2020-10-26 18:04:30 +0100

    Fix sh syntax

--- publish_images.sh
+++ publish_images.sh
@@ -6,6 +6,5 @@ echo "--- Pushing images from modules ---"
 for f in $FILES
 do
 	echo "Processing module $f"
-	echo "---111 $f/publish_image.sh $GH_PAT"
-	sh "$f/publish_image.sh $GH_PAT"
+	sh "$f/publish_image.sh" "$GH_PAT"
 done
