2148060d25cc61e4fe72f6ae5a8a66beb464c286
Jesus <jesusd.alvarado@gmail.com>
2020-10-27 09:59:09 +0100

    Fix passing GH_PAT to script

--- modules/webserver/publish_image.sh
+++ modules/webserver/publish_image.sh
@@ -1,7 +1,5 @@
 GH_PAT=$1
 
-echo "---111"
-echo $GH_PAT
 docker build ./modules/webserver/ -t jesusdalvarado-example-image:latest
 echo $GH_PAT | docker login ghcr.io -u jesusdalvarado --password-stdin
 docker tag jesusdalvarado-example-image:latest ghcr.io/jesusdalvarado/jesus-image:latest
